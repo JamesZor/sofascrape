7bcab0735cebf6058f3b2c5d21b2f643d4cf44b8
James LP1 <jameszoryk@msn.com>
2025-10-07 16:04:25 +0100

    loader init updated

---
+++ src/sofascrape/loader/__init__.py
@@ -0,0 +1,5 @@
+from .footballDataManager import FootballDataManager
+from .footballloader import FootballLoader
+from .footballTransformer import FootballDataTransformer
+
+__all__ = ["FootballLoader", "FootballDataManager", "FootballDataTransformer"]
