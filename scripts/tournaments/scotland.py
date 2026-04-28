a75e8f4081320d91cd4691ab5a0051b7b2ecaca3
James LP1 <jameszoryk@msn.com>
2026-04-28 21:40:13 +0100

    scotland pl and ch

--- scripts/tournaments/scotland.py
+++ scripts/tournaments/scotland.py
@@ -24,43 +24,49 @@ eliteserien_tour_id = 54
 division_1st_tour_id = 55
 
 
-pipeline.setup_tournament(eliteserien_tour_id)
-pipeline.setup_tournament(division_1st_tour_id)
-
-pipeline.setup_tournament(eliteserien_tour_id)
-pipeline.setup_tournament(division_1st_tour_id)
+# pipeline.setup_tournament(eliteserien_tour_id)
+# pipeline.setup_tournament(division_1st_tour_id)
 #
-
-list_season_ids_l1 = get_seasonid_year_from_tournament(
-    pipeline=pipeline, tournament_id=eliteserien_tour_id, result_limit=6
-)
-
-list_season_ids_l2 = get_seasonid_year_from_tournament(
-    pipeline=pipeline, tournament_id=division_1st_tour_id, result_limit=6
-)
-
-
-target_components = [
-    Component.BASE,
-    Component.STATS,
-    Component.ODDS,
-    Component.LINEUPS,
-    Component.INCIDENTS,
-]
-
-base_one = queue_list_of_seasons(
-    season_tournament_list=list_season_ids_l1,
-    target_components=target_components,
-    pipeline=pipeline,
-)
-
-base_two = queue_list_of_seasons(
-    season_tournament_list=list_season_ids_l2,
-    target_components=target_components,
-    pipeline=pipeline,
-)
-
+# pipeline.setup_tournament(eliteserien_tour_id)
+# pipeline.setup_tournament(division_1st_tour_id)
+# #
+#
+# list_season_ids_l1 = get_seasonid_year_from_tournament(
+#     pipeline=pipeline, tournament_id=eliteserien_tour_id, result_limit=6
+# )
+#
+# list_season_ids_l2 = get_seasonid_year_from_tournament(
+#     pipeline=pipeline, tournament_id=division_1st_tour_id, result_limit=6
+# )
+#
+#
+# target_components = [
+#     Component.BASE,
+#     Component.STATS,
+#     Component.ODDS,
+#     Component.LINEUPS,
+#     Component.INCIDENTS,
+# ]
+#
+# base_one = queue_list_of_seasons(
+#     season_tournament_list=list_season_ids_l1,
+#     target_components=target_components,
+#     pipeline=pipeline,
+# )
+#
+# base_two = queue_list_of_seasons(
+#     season_tournament_list=list_season_ids_l2,
+#     target_components=target_components,
+#     pipeline=pipeline,
+# )
+#
 pipeline.run_worker_loop(
     max_workers=config.pipeline.max_workers,
     task_limit=6000,  # <-- Let it run until it's finished!
+)
+
+
+pipeline.retry_failed_components()
+pipeline.run_worker_loop(
+    max_workers=2, task_limit=2000  # Just need 1 or 2 workers for a quick cleanup
 )
